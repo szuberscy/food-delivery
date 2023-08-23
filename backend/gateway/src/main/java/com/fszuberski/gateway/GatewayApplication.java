702e9a74fde91c9b471c07e6c458c014715cb83c
Filip Szuberski <filip.szuberski@gmail.com>
2023-08-23 21:42:53 +0100

    be/gateway: add gateway service

---
+++ backend/gateway/src/main/java/com/fszuberski/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.fszuberski.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+// https://spring.io/guides/gs/gateway/
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("users", r -> r
+                        .path("/v1/users/**")
+                        .uri("http://localhost:8081")
+                )
+                .build();
+    }
+
+}
