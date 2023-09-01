3a2b3756773bc17677d5ddef3e0b1170991a8149
Filip Szuberski <filip.szuberski@gmail.com>
2023-09-01 22:05:46 +0100

    be/user: add openapi docs

--- backend/user/src/main/kotlin/com/fszuberski/fooddelivery/registration/common/plugins/Routing.kt
+++ backend/user/src/main/kotlin/com/fszuberski/fooddelivery/registration/common/plugins/Routing.kt
@@ -2,11 +2,13 @@ package com.fszuberski.fooddelivery.registration.common.plugins
 
 import com.fszuberski.fooddelivery.registration.adapter.rest.userRouting
 import io.ktor.server.application.*
+import io.ktor.server.plugins.openapi.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.get
 
 fun Application.configureRouting() {
     routing {
+        openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
         userRouting(registerUserUseCase = get())
     }
 }
