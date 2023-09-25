c1308709b89100667d4b89f3fba58693733d3e87
Filip Szuberski <filip.szuberski@gmail.com>
2023-09-25 08:56:21 +0100

    be: add location and restaurant services

---
+++ backend/restaurant/src/main/kotlin/com/fszuberski/Application.kt
@@ -0,0 +1,14 @@
+package com.fszuberski
+
+import com.fszuberski.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
